219434a6f6404fb2a4e66e06b9326474bcf56da1
ArturN <nizamutdin.art@yandex.ru>
2016-12-10 15:22:31 +0300

    Added material-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class App extends Component {
   render() {
@@ -13,6 +14,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <RaisedButton className="m15" label="Default" />
+          <RaisedButton className="m15" label="Primary" primary={true} />
+          <RaisedButton className="m15" label="Secondary" secondary={true} />
+          <RaisedButton className="m15" label="Disabled" disabled={true} />
+          <br />
+          <br />
+          <RaisedButton label="Full width" fullWidth={true} />
+        </div>
       </div>
     );
   }
