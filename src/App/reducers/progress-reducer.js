668c20320ba498b4b5cac5becf080966c267e3f9
ArturN <nizamutdin.art@yandex.ru>
2017-01-14 18:03:00 +0400

    progress-bar: added processing of REMOVE_TODO action

--- src/App/reducers/progress-reducer.js
+++ src/App/reducers/progress-reducer.js
@@ -3,15 +3,26 @@ import {store} from '../store/store';
 import * as _ from 'lodash';
 
 export default (state = 100, action) => {
-  if (action.type !== types.ADD_TODO && action.type !== types.UPDATE_TODO) return state;
+  let actionType = action.type;
+  if (
+    actionType !== types.ADD_TODO && 
+    actionType !== types.UPDATE_TODO &&
+    actionType !== types.REMOVE_TODO
+  ) return state;
 
   const todoList = store.getState().todoList.present;
   const isDoneTodoList = todoList.filter(todo => todo.isDone);
 
-  switch (action.type) {
+  switch (actionType) {
     case types.ADD_TODO:
       return isDoneTodoList.length / (todoList.length + 1) * 100;
-    case types.UPDATE_TODO:
+    case types.REMOVE_TODO: {
+      let {todo} = action;
+      let shiftIsDone = +todo.isDone;
+
+      return (isDoneTodoList.length - shiftIsDone) / (todoList.length - 1)* 100;
+    }
+    case types.UPDATE_TODO: {
       let relatedTodo = _.find(todoList, todo => todo.id === action.todo.id);
       let shiftIsDone = 0;
 
@@ -20,6 +31,7 @@ export default (state = 100, action) => {
       }
 
       return (isDoneTodoList.length + shiftIsDone) / todoList.length * 100;
+    }
     default:
       return state;
   }
