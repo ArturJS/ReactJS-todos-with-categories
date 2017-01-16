cc5e0eab04ffffeff059760c9c56b88a322dfb70
ArturN <nizamutdin.art@yandex.ru>
2017-01-16 15:43:49 +0400

    Added unit test for Todo component

---
+++ src/App/pages/TodoListPage/Todo.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {mount, render} from 'enzyme';
+import sinon from 'sinon';
+import * as _ from 'lodash';
+import shortid from 'shortid';
+
+import Todo from './Todo';
+
+describe('<Todo />', () => {
+  it('<Todo /> component should render whole data properly', () => {
+    const todo = {
+      id: shortid.generate(),
+      title: '123',
+      categoryId: shortid.generate(),
+      isDone: true
+    };
+
+    const wrapper = render(<Todo todo={todo} removeTodo={_.noop} updateTodo={_.noop}/>);
+
+    expect(wrapper.find('.todo-title-link').text()).toBe('123');
+    expect(wrapper.find('.chbx').get(0).attribs.checked).toBeDefined();
+  });
+
+
+  it('<Todo /> component should invoke updateTodo and removeTodo methods', () => {
+    const todo = {
+      id: shortid.generate(),
+      title: '123',
+      categoryId: shortid.generate(),
+      isDone: true
+    };
+    const onRemoveTodo = sinon.spy();
+    const onUpdateTodo = sinon.spy();
+
+    const wrapper = mount(<Todo todo={todo} removeTodo={onRemoveTodo} updateTodo={onUpdateTodo}/>);
+
+    wrapper.find('.todo-remove').simulate('click');
+    wrapper.find('.chbx').simulate('change');
+
+    expect(onRemoveTodo.callCount).toBe(1);
+    expect(onUpdateTodo.callCount).toBe(1);
+  });
+});
