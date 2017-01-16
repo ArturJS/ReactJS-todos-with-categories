a87af303e97341df0084df31200412101c9bfd73
ArturN <nizamutdin.art@yandex.ru>
2017-01-16 15:54:23 +0400

    Added unit test for TodoList component

---
+++ src/App/pages/TodoListPage/TodoList.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {render} from 'enzyme';
+import * as _ from 'lodash';
+import shortid from 'shortid';
+
+import Todo from './Todo';
+import TodoList from './TodoList';
+
+describe('<TodoList />', () => {
+  it('<TodoList /> component should render 5 Todo components', () => {
+    const todoList = _.times(5, ()=> {
+      return {
+        id: shortid.generate(),
+        title: '123',
+        categoryId: shortid.generate(),
+        isDone: true
+      };
+    });
+
+    const wrapper = render(<TodoList todoList={todoList} removeTodo={_.noop} updateTodo={_.noop}/>);
+
+    expect(wrapper.find('.todo-card').length).toBe(5);
+  });
+
+  it('<TodoList /> component should render "Nothing to display..." text', () => {
+    const wrapper = render(<TodoList todoList={[]} removeTodo={_.noop} updateTodo={_.noop}/>);
+
+    expect(wrapper.text()).toBe('Nothing to display...');
+  });
+});
