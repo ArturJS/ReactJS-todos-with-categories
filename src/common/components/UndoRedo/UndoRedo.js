ab453d705f3e22c9a4074a2988314e85b7fa62b6
ArturJS <nizamutdin.art@yandex.ru>
2017-10-15 11:26:51 +0400

    Improved UndoRedo component

--- src/common/components/UndoRedo/UndoRedo.js
+++ src/common/components/UndoRedo/UndoRedo.js
@@ -1,24 +1,54 @@
 import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 import {ActionCreators} from 'redux-undo';
-import store from '../../store';
 import './UndoRedo.scss';
 
+function mapStateToProps(state, props) {
+  return {
+    orm: state.orm
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    undoRedoActions: bindActionCreators(ActionCreators, dispatch)
+  };
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class UndoRedo extends PureComponent {
+  static propTypes = {
+    orm: PropTypes.object.isRequired,
+    undoRedoActions: PropTypes.object.isRequired
+  };
+
   undo = () => {
-    store.dispatch(ActionCreators.undo());
+    this.props.undoRedoActions.undo();
   };
 
   redo = () => {
-    store.dispatch(ActionCreators.redo());
+    this.props.undoRedoActions.redo();
   };
 
   render() {
+    const {orm} = this.props;
+
     return (
       <div className="undo-redo-cnt">
-        <button type="button" className="btn btn-default" onClick={this.undo}>
+        <button
+          type="button"
+          className="btn btn-default"
+          disabled={orm.past.length === 0}
+          onClick={this.undo}>
           <i className="glyphicon glyphicon-arrow-left"/>
         </button>
-        <button type="button" className="btn btn-default" onClick={this.redo}>
+        <button
+          type="button"
+          className="btn btn-default"
+          disabled={orm.future.length === 0}
+          onClick={this.redo}>
           <i className="glyphicon glyphicon-arrow-right"/>
         </button>
       </div>
